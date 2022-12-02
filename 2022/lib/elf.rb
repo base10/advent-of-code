a2956fe9624135df2a605792ba0cac5cdb7a922d
Nathan L. Walls <nathan@wallscorp.us>
2022-12-01 21:46:08 -0500

    Taping out

---
+++ 2022/lib/elf.rb
@@ -0,0 +1,7 @@
+class Elf
+  attr_accessor :calories
+
+  def initialize(calories:)
+    @calories = calories
+  end
+end
---
+++ 2022/scripts/day1-puzzle1.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require_relative "../lib/payload_parser.rb"
+require_relative "../lib/elf.rb"
+
+input = "../inputs/day1_sample.txt"
