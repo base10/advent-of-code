a4662db5c5ea5fac74b1b8fdf956510db25d7668
Nathan L. Walls <nathan@wallscorp.us>
2022-12-06 23:22:25 -0500

    Day 4, Puzzle 1

---
+++ 2022/scripts/day4.rb
@@ -0,0 +1,77 @@
+#!/usr/bin/env ruby
+
+require "pry-byebug"
+
+class Day4
+  attr_accessor :file_lines, :input_file, :puzzle1_result, :puzzle2_result
+
+  def initialize(input_file:)
+    @input_file = input_file
+  end
+
+  def run
+    read_file
+    puzzle1
+    puzzle2
+  end
+
+  def report
+    [puzzle1_report, puzzle2_report].join("\n")
+  end
+
+  private
+
+  def read_file
+    @file_lines = File.readlines(input_file, chomp: true)
+  end
+
+  def puzzle1
+    data = []
+    file_lines.each do |line|
+      tuple_a, tuple_b = line.split(",")
+
+      range_a = tuple_to_range(tuple: tuple_a)
+      range_b = tuple_to_range(tuple: tuple_b)
+
+      range_array = [range_a, range_b]
+
+      next unless range_contains_other?(ranges: range_array)
+
+      data << range_array
+    end
+
+    @puzzle1_result = { count: data.count }
+  end
+
+  def puzzle1_report
+    "Puzzle 1: Ranges containing the paired range: #{puzzle1_result[:count]}"
+  end
+
+  def puzzle2
+  end
+
+  def puzzle2_report
+    "Puzzle 2: Nothing yet"
+  end
+
+  def tuple_to_range(tuple:)
+    a, b = tuple.split("-")
+
+    (a.to_i..b.to_i)
+  end
+
+  def range_contains_other?(ranges:)
+    range_a, range_b = ranges
+
+    range_a.cover?(range_b) || range_b.cover?(range_a)
+  end
+end
+
+input_file_name = ARGV.shift
+input_file = File.path(Pathname.new(Dir.pwd + "/inputs/#{input_file_name}"))
+
+day4 = Day4.new(input_file: input_file)
+
+day4.run
+
+puts day4.report
